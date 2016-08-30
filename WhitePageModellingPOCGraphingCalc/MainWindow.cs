2e4ee2b5f641bd02ce2148747c1a253ad7279c44
mkonkolowicz <mkonkolowicz@gmail.com>
2016-08-29 21:50:11 -0400

    Added a new set of POC tests

---
+++ WhitePageModellingPOCGraphingCalc/MainWindow.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using TestStack.White;
+using TestStack.White.UIItems;
+
+namespace WhitePageModellingPOCGraphingCalc
+{
+    [SetUpFixture]
+    public class TestHelper
+    {
+        const string localPathToTestApp = "C:\\temp\\";
+        public TestStack.White.UIItems.WindowItems.Window appWindow
+        {
+             
+            get
+            {
+               return Application.Launch(localPathToTestApp+"GraphingCalculatorDemo.exe").GetWindow("MainWindow");
+            }
+        }
+        public MainWindow window { get { return new MainWindow(); } } 
+    }
+
+    [TestFixture]
+    public class MainWindowShould:TestHelper
+    {
+        [Test]
+        public void Appear()
+        {
+            Assert.That(appWindow.Visible);
+        }
+        [Test]
+        public void TurnsOff()
+        {
+            window.Off.Click();
+            Assert.That(!window.Off.Enabled);
+        }
+    }
+
+    public class MainWindow : TestHelper
+    {
+        public Button Off { get { return appWindow.Get<Button>("UIOff"); } }
+    }
+}
