6954b804a2f08b08ff02e5018ae26c1bd756193e
mkonkolowicz <mkonkolowicz@gmail.com>
2016-08-30 20:30:53 -0400

    Added more tests

--- WhitePageModellingPOCGraphingCalc/MainWindow.cs
+++ WhitePageModellingPOCGraphingCalc/MainWindow.cs
@@ -1,42 +1,58 @@
 ﻿using NUnit.Framework;
+using System;
 using TestStack.White;
 using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
 
 namespace WhitePageModellingPOCGraphingCalc
 {
-    [SetUpFixture]
     public class TestHelper
     {
         const string localPathToTestApp = "C:\\temp\\";
-        public TestStack.White.UIItems.WindowItems.Window appWindow
-        {
-             
-            get
-            {
-               return Application.Launch(localPathToTestApp+"GraphingCalculatorDemo.exe").GetWindow("MainWindow");
-            }
-        }
-        public MainWindow window { get { return new MainWindow(); } } 
-    }
+        private static Application app = Application.Launch(localPathToTestApp + "GraphingCalculatorDemo.exe");
+        public static Window appWindow = app.GetWindow("MainWindow");
+    }   
 
     [TestFixture]
     public class MainWindowShould:TestHelper
     {
+        private MainWindowModel mainWindow = new MainWindowModel();
+
         [Test]
         public void Appear()
         {
             Assert.That(appWindow.Visible);
         }
         [Test]
-        public void TurnsOff()
+        public void ShowSin()
         {
-            window.Off.Click();
-            Assert.That(!window.Off.Enabled);
+            Assert.That(mainWindow.Sin.Visible);
+        }
+
+        [Test]
+        public void ShowTan()
+        {
+            Assert.That(mainWindow.Sin.Visible);
+        }
+
+        [Test]
+        public void ShowCos()
+        {
+            Assert.That(mainWindow.Sin.Visible);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            appWindow.Close();
         }
     }
 
-    public class MainWindow : TestHelper
+    public class MainWindowModel : TestHelper
     {
         public Button Off { get { return appWindow.Get<Button>("UIOff"); } }
+        public Button Sin { get { return appWindow.Get<Button>("UIAppendSin"); } }
+        public Button Cos { get { return appWindow.Get<Button>("UIAppendCos"); } }
+        public Button Tan { get { return appWindow.Get<Button>("UIAppendTan"); } }
     }
 }
